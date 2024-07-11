c2977a2fef351805923418a1d3d85f45703eb549
duangha-thai <duanghathai.yar@rmutto.ac.th>
2024-07-11 16:08:25 +0700

    Week02 Cards import export

---
+++ app/routes/Data.tsx
@@ -0,0 +1,32 @@
+export const cards = [
+    {
+        id: 100,
+        name: "Duanghathai",
+        biog: "นักเรียน",
+        bgProf: "/images/3.jpg",
+        userIcon: "/images/4.jpg",
+        userName: "Tong",
+        createdAt: "Feb 21, 2003",
+        active: true
+    },
+    {
+        id: 101,
+        name: "Chompunut",
+        biog: "นักเรียน",
+        bgProf: "/images/5.jpg",
+        userIcon: "/images/1.jpg",
+        userName: "Chompu",
+        createdAt: "Dec 28, 2002",
+        active: false
+    },
+    {
+        id: 102,
+        name: "Pimphika",
+        biog: "นักเรียน",
+        bgProf: "/images/4.jpg",
+        userIcon: "/images/2.jpg",
+        userName: "June",
+        createdAt: "Feb 12, 2003",
+        active: true
+    }
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,57 +1,61 @@
 import type { MetaFunction } from "@remix-run/node";
+import MyCards from "./MyCards";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Duanghathai Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <hr className="m-4"/>
-      <h1 className="text-3xl">My Profile</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
+    
+      <MyCards />
+    //   <div className="font-sans p-4"></div>
+    // {/* <h1 className="text-3xl">Welcome to Remix</h1>
+    //   <ul className="list-disc mt-4 pl-6 space-y-2">
+    //     <li>
+    //       <a
+    //         className="text-blue-700 underline visited:text-purple-900"
+    //         target="_blank"
+    //         href="https://remix.run/start/quickstart"
+    //         rel="noreferrer"
+    //       >
+    //         5m Quick Start
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         className="text-blue-700 underline visited:text-purple-900"
+    //         target="_blank"
+    //         href="https://remix.run/start/tutorial"
+    //         rel="noreferrer"
+    //       >
+    //         30m Tutorial
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         className="text-blue-700 underline visited:text-purple-900"
+    //         target="_blank"
+    //         href="https://remix.run/docs"
+    //         rel="noreferrer"
+    //       >
+    //         Remix Docs
+    //       </a>
+    //     </li>
+    //   </ul>
+    //   <hr className="m-4"/>
+    //   <h1 className="text-3xl">My Profile</h1>
+    //   <ul className="list-disc mt-4 pl-6 space-y-2"> 
         
-        <li>Name : Duanghathai Yarangpai</li>
-        <li>Class : Informmation Technology </li>
-        <li>E-mail : <a href="mailto:Duanghathai_yar@rmutto.ac.th">Contact me.</a></li>
-        <li> <img src="img/01.jpg" alt="duang" /></li>
-      </ul>
-    </div>
+    //     <li>Name : Duanghathai Yarangpai</li>
+    //     <li>Class : Informmation Technology </li>
+    //     <li>E-mail : <a href="mailto:Duanghathai_yar@rmutto.ac.th">Contact me.</a></li>
+    //     <li> <img src="img/01.jpg" alt="duang" /></li>
+    //   </ul>
+    // </div>  */}
+
   );
 }
